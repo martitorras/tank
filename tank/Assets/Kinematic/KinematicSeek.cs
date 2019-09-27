ce426ab84778195f70f6a5627865376c610941e8
Martí Torras Isanta <martiti@TR12.CAT>
2019-09-27 12:01:53 +0200

    tank game

---
+++ tank/Assets/Kinematic/KinematicSeek.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class KinematicSeek : MonoBehaviour {
+
+	Move move;
+
+	// Use this for initialization
+	void Start () {
+		move = GetComponent<Move>();
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+        // TODO 1: Set movement velocity to max speed in the direction of the target
+        Vector3 kill_me = move.target.transform.position - move.transform.position;
+        kill_me.Normalize();
+        move.SetMovementVelocity(kill_me * move.max_mov_velocity);
+        
+        // Remember to enable this component in the inspector
+    }
+}
